12513074919caa4aae0685415d35b25eb38c5e25
Ashwani Katiyar <96191479+ashwanikatiyar@users.noreply.github.com>
2026-09-11 15:10:32 -0500

    Time: 0 ms (100%), Space: 13 MB (8.03%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,18 @@
+func maxProfit(prices []int) int {
+    min_price := prices[0]
+    max_profit := 0
+
+    for _ , price := range prices{
+        if price < min_price{
+            min_price = price
+        }else {
+            profit := price - min_price
+            if profit > max_profit{
+                max_profit = profit
+            }
+        }
+
+
+    }
+    return max_profit
+}
