696b66201d9ee52c23e551853cddd6920338a9a3
Ashwani Katiyar <96191479+ashwanikatiyar@users.noreply.github.com>
2026-09-11 09:50:26 -0500

    Time: 0 ms (100%), Space: 6.2 MB (84.2%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -6,34 +6,46 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-
-    result := &ListNode{}    
-    current := result
+    dummy := &ListNode{} // Start with dummy node (It give us easy starting point)
+    cur := dummy
     carry := 0
 
-    for l1 != nil || l2 != nil || carry != 0{
+    // Keep going while there is something to add
+    for l1 != nil || l2 != nil || carry != 0 {
 
-        val1 := 0
+        // Get current digits (0 if list ended)
+        a, b := 0, 0
         if l1 != nil {
-            val1 = l1.Val
+            a = l1.Val
             l1 = l1.Next
         }
-
-        val2 := 0
-        if l2 != nil{
-            val2 = l2.Val
+        if l2 != nil {
+            b = l2.Val
             l2 = l2.Next
         }
 
-        sum := val1 + val2 + carry
+        // Add digits + carry
+        sum := a + b + carry
 
+        // Current digit + carry for next round
         digit := sum % 10
         carry = sum / 10
 
-        current.Next = &ListNode{Val : digit}
-        current = current.Next
+        // Add digit to result
+        cur.Next = &ListNode{Val: digit}
+        cur = cur.Next
     }
 
-    return result.Next
-
+    return dummy.Next // Skip dummy and return the next one
 }
+
+// 🧠 Remember the pattern
+// Every loop:
+
+// get → add → digit → carry → attach
+
+// Get a and b
+// Add a + b + carry
+// Digit = sum % 10
+// Carry = sum / 10
+// Attach digit to result
